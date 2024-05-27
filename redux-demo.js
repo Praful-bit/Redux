eaf8ebcc225926b8db8cc8b80f5871f0c5857ec7
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-05-27 18:30:19 +0530

    increment and decrement

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,18 @@
 const redux = require ('redux')
 
-const counterReducer =(state ={counter:0},action)=>{
-    return{
-        counter: state.counter+1
-    }
+const counterReducer =(state ={counter:0}, action)=>{
+  
+      if(action.type === 'increment'){
+           return{
+               counter: state.counter + 1
+           }
+      }
+      if(action.type === 'decrement'){
+        return{
+            counter: state.counter - 1
+        }
+      }
+    
 }
 
 const store = redux.createStore(counterReducer)
