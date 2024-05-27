6eea64a7f3102431f8498a996321826e500d932f
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-05-27 18:44:57 +0530

    nothing

--- redux-demo.js
+++ redux-demo.js
@@ -2,25 +2,25 @@ const redux = require ('redux')
 
 const counterReducer =(state ={counter:0}, action)=>{
   
-      if(action.type === 'increment'){
-           return{
-               counter: state.counter + 1
-           }
-      }
-      if(action.type === 'decrement'){
-        return{
-            counter: state.counter - 1
-        }
-      }
-    if(action.type === 'INCREMENTBY2'){
-        return{
-            counter: state.counter+2
-        }
-    }
-    if(action.type === "DECREMENTBY2"){
-        return{
-            counter : state.counter-2
-        }
+    switch (action.type) {
+        case 'increment':
+            return {
+                counter: state.counter + 1,
+            };
+        case 'decrement':
+            return {
+                counter: state.counter - 1,
+            };
+        case 'INCREMENTBY2':
+            return {
+                counter: state.counter + 2,
+            };
+        case 'DECREMENTBY2':
+            return {
+                counter: state.counter - 2,
+            };
+        default:
+            return state;
     }
 }
 
